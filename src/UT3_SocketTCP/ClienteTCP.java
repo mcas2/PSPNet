c3541c1c1fcdf523c7d1f61e07a7d89c21e338da
mcas2 <mcas2@tutanota.com>
2022-01-12 12:19:00 +0100

    Functional client

--- src/UT3_SocketTCP/ClienteTCP.java
+++ src/UT3_SocketTCP/ClienteTCP.java
@@ -7,28 +7,42 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClienteTCP {
-    static final String HOST = "127.0.0.1";
-    static final int PORT = 2222;
+    static final String HOST = "192.168.43.67";
+    static final int PORT = 1024;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         DataInputStream in;
         DataOutputStream out;
+        System.out.println("...");
+        String mensajePersonalizado = scanner.nextLine();
 
         Socket sc = null;
 
-        try {
-            sc = new Socket(HOST, PORT);
-            System.out.println("Cliente conectado");
+        for (int i = 0; i < 100000; i++) {
+            try {
+                sc = new Socket(HOST, PORT);
+                System.out.println("Cliente conectado");
 
-            in = new DataInputStream(sc.getInputStream());
-            out = new DataOutputStream(sc.getOutputStream());
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+                out.writeUTF(mensajePersonalizado);
+                String mensaje = in.readUTF();
+                System.out.println("Mensaje recibido del servidor: "+mensaje);
+
+                in.close();
+                out.close();
+                sc.close();
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/UT3_SocketTCP/ServidorTCP.java
+++ src/UT3_SocketTCP/ServidorTCP.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class ServidorTCP {
-    static final int PORT = 2222;
+    static final int PORT = 1024;
 
     public static void main(String[] args) {
         //Flujo de mensajes
