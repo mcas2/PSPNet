ef947aa5f9851c7d2ac971eab1f024d85df2637e
mcas2 <mcas2@tutanota.com>
2022-02-09 11:58:41 +0100

    EJ 2 UDP

---
+++ src/UT3_P2_E2/Cliente.java
@@ -0,0 +1,52 @@
+package UT3_P2_E2;
+
+import java.io.IOException;
+import java.net.*;
+import java.sql.Time;
+
+public class Cliente {
+    final static int PORT_SERVER = 5000;
+
+    public static void main(String[] args) {
+        byte [] buffer;
+
+        try {
+            InetAddress serverAddress  = InetAddress.getLocalHost();
+
+            DatagramSocket ds = new DatagramSocket();
+            ds.setSoTimeout(5000);
+
+            String mensaje = "Petición";
+
+            buffer = mensaje.getBytes();
+            buffer = new byte[1024];
+
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length, serverAddress, PORT_SERVER);
+
+            ds.send(peticion);
+
+
+            long segundosInicio = System.currentTimeMillis();
+            long segundosActuales = 0;
+
+
+
+            DatagramPacket respuestaServidor = new DatagramPacket(buffer, buffer.length);
+
+            ds.receive(respuestaServidor);
+
+            String respuesta = new String(respuestaServidor.getData());
+            System.out.println(respuesta);
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        catch (SocketTimeoutException e){
+            System.out.println("El cliente no ha recibido respuesta del servidor.");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/UT3_P2_E2/Servidor.java
@@ -0,0 +1,48 @@
+package UT3_P2_E2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Servidor {
+    final static int PORT = 5000;
+
+    public static void main(String[] args) {
+        byte [] buffer = new byte[1024];
+        LocalDateTime now = LocalDateTime.now();
+
+        try {
+            System.out.println("Inicializando el servidor UDP");
+            DatagramSocket socketUDP = new DatagramSocket(PORT);
+            String mensaje;
+
+            while (true){
+                DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+                socketUDP.receive(peticion);
+
+                mensaje = new String(peticion.getData());
+                System.out.println("Recibiendo la petición del cliente...");
+
+                int puertoCliente = peticion.getPort();
+                InetAddress address = peticion.getAddress();
+
+                String msgRespuesta = now.toString();
+                buffer = msgRespuesta.getBytes();
+
+                //DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, address, puertoCliente);
+                //socketUDP.send(respuesta);
+                //System.out.println("Enviada la respuesta");
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
